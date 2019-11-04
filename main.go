6b218a5f15c23b9b40641894c3fbb73049c59812
Musashi Sakamoto <1292602b@gmail.com>
2019-11-04 23:05:16 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/Musashi-Sakamoto/fullstack/api"
+
+func main() {
+	api.Run()
+}
---
+++ tests/controllertests/user_controller_test.go
@@ -0,0 +1,181 @@
+package controllertests
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+
+	"github.com/Musashi-Sakamoto/fullstack/api/models"
+	"github.com/gorilla/mux"
+	"gopkg.in/go-playground/assert.v1"
+)
+
+func TestCreateUser(t *testing.T) {
+	err := refreshUserTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	samples := []struct {
+		inputJSON    string
+		statusCode   int
+		nickname     string
+		email        string
+		errorMessage string
+	}{
+		{
+			inputJSON:    `{"nickname":"Pet", "email": "pet@gmail.com", "password": "password"}`,
+			statusCode:   201,
+			nickname:     "Pet",
+			email:        "pet@gmail.com",
+			errorMessage: "",
+		},
+		{
+			inputJSON:    `{"nickname":"Frank", "email": "pet@gmail.com", "password": "password"}`,
+			statusCode:   500,
+			errorMessage: "Email Already Taken",
+		},
+		{
+			inputJSON:    `{"nickname":"Pet", "email": "grand@gmail.com", "password": "password"}`,
+			statusCode:   500,
+			errorMessage: "Nickname Already Taken",
+		},
+		{
+			inputJSON:    `{"nickname":"Kan", "email": "kangmail.com", "password": "password"}`,
+			statusCode:   422,
+			errorMessage: "Invalid Email",
+		},
+		{
+			inputJSON:    `{"nickname": "", "email": "kan@gmail.com", "password": "password"}`,
+			statusCode:   422,
+			errorMessage: "Required Nickname",
+		},
+		{
+			inputJSON:    `{"nickname": "Kan", "email": "", "password": "password"}`,
+			statusCode:   422,
+			errorMessage: "Required Email",
+		},
+		{
+			inputJSON:    `{"nickname": "Kan", "email": "kan@gmail.com", "password": ""}`,
+			statusCode:   422,
+			errorMessage: "Required Password",
+		},
+	}
+
+	for _, v := range samples {
+		req, err := http.NewRequest("POST", "/users", bytes.NewBufferString(v.inputJSON))
+		if err != nil {
+			t.Errorf("this is the error: %v", err)
+		}
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(server.CreateUser)
+		handler.ServeHTTP(rr, req)
+
+		responseMap := make(map[string]interface{})
+		err = json.Unmarshal([]byte(rr.Body.String()), &responseMap)
+		if err != nil {
+			t.Errorf("this is the error: %v", err)
+		}
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(server.CreateUser)
+		handler.ServeHTTP(rr, req)
+
+		responseMap := make(map[string]interface{})
+		err = json.Unmarshal([]byte(rr.Body.String()), &responseMap)
+		if err != nil {
+			fmt.Printf("Cannot convert to json: %v", err)
+		}
+		assert.Equal(t, rr.Code, v.statusCode)
+		if v.statusCode == 201 {
+			assert.Equal(t, responseMap["nickname"], v.nickname)
+			assert.Equal(t, responseMap["email"], v.email)
+		}
+
+		if v.statusCode == 422 || v.statusCode == 500 && v.errorMessage != "" {
+			assert.Equal(t, responseMap["error"], v.errorMessage)
+		}
+	}
+}
+
+func TestGetUsers(t *testing.T) {
+	err := refreshUserTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, err = seedUsers()
+	if err != nil {
+		log.Fatal(err)
+	}
+	req, err := http.NewRequest("GET", "/users", nil)
+	if err != nil {
+		t.Errorf("this is the error: %v\n", err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(server.GetUsers)
+	handler.ServeHTTP(rr, req)
+
+	var users []models.User
+	err = json.Unmarshal([]byte(rr.Body.String()), &users)
+	if err != nil {
+		log.Fatalf("Cannot convert to json: %v\n", err)
+	}
+	assert.Equal(t, rr.Code, http.StatusOK)
+	assert.Equal(t, len(users), 2)
+}
+
+func TestGetUserByID(t *testing.T) {
+	err := refreshUserTable()
+	if err != nil {
+		log.Fatal(err)
+	}
+	user, err := seedOneUser()
+	if err != nil {
+		log.Fatal(err)
+	}
+	userSample := []struct {
+		id           string
+		statusCode   int
+		nickname     string
+		email        string
+		errorMessage string
+	}{
+		{
+			id:         strconv.Itoa(int(user.ID)),
+			statusCode: 200,
+			nickname:   user.Nickname,
+			email:      user.Email,
+		},
+		{
+			id:         "unknown",
+			statusCode: 400,
+		},
+	}
+
+	for _, v := range userSample {
+		req, err := http.NewRequest("GET", "/users", nil)
+		if err != nil {
+			t.Errorf("This is the error: %v\n", err)
+		}
+		req = mux.SetURLVars(req, map[string]string{"id": v.id})
+		rr := httptest.NewRecorder()
+		handler := http.HandlerFunc(server.GetUser)
+		handler.ServeHTTP(rr, req)
+
+		responseMap := make(map[string]interface{})
+		err = json.Unmarshal([]byte(rr.Body.String()), &responseMap)
+		if err != nil {
+			log.Fatalf("Cannot convert to json: %v", err)
+		}
+
+		assert.Equal(t, rr.Code, v.statusCode)
+
+		if v.statusCode == 200 {
+			assert.Equal(t, user.Nickname, responseMap["nickname"])
+			assert.Equal(t, user.Email, responseMap["email"])
+		}
+	}
+}
